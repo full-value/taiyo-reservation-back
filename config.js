2a57ef796d2fe0a9ca56ffc955045f1a6199a533
webstar923 <webstar1987923@hotmail.com>
2025-03-05 11:25:26 +0900

    comment modification

--- config.js
+++ config.js
@@ -2,6 +2,8 @@ const { Sequelize } = require('sequelize');
 
 // Set up Sequelize with your MySQL credentials
 const sequelize = new Sequelize('reservation_system', 'root', 'GameT!me0106', {
+// const sequelize = new Sequelize('reservation_system', 'root', '', {
+
   host: '127.0.0.1',
   dialect: 'mysql',
 });
--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const { Sequelize,DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize('reservation_system', 'root', 'GameT!me0106', {
+// const sequelize = new Sequelize('reservation_system', 'root', '', {
+
   host: '127.0.0.1',
   port:3306,
   dialect: 'mysql',
